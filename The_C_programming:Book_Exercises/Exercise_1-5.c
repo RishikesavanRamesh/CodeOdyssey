6f765fc9b0edb2b476569c536f616a4244179f4e
RishikesavanRamesh <automationwith.rishikesavan@gmail.com>
2023-06-23 12:27:56 +0530

    complete Ex_1-5

---
+++ The_C_programming:Book_Exercises/Exercise_1-5.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+float celcius_to_fahr(float celsius);
+
+void main(){
+  float fahr, celsius;
+  int lower = 0, step = 20, upper = 300;
+
+  printf("Celsius Fahr.\n");
+  for(celsius=upper; celsius >= lower; celsius = celsius - step)
+    printf("%7.1f %5.0f\n", celsius, celcius_to_fahr(celsius));
+}
+
+float celcius_to_fahr(float celsius){
+  return ((celsius*9.0/5.0) + 32.0);
+}
